7f7007e0d7983735f9980cdda05f9e7b2f80ed49
DieryValencia <diery.valencia@campusucc.edu.co>
2025-11-12 10:39:30 -0500

    direccionamiento

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "../components/Layout";
 
 // Views
@@ -15,11 +15,12 @@ export default function AppRoutes() {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />        
         <Route path="layouts" element={<LayoutsView />} />
-        <Route path="matematicas/descomposicion" element={<Descomposicion />} />
-        <Route path="ciencias-naturales" element={<CienciasNaturales />} />
-        <Route path="ciclo-del-agua" element={<CicloDelAgua />} />
-        <Route path="ciencias-sociales" element={<CienciasSociales />} />
-      </Route>
-    </Routes>
+          <Route path="matematicas/descomposicion" element={<Descomposicion />} />
+          <Route path="ciencias-naturales" element={<CienciasNaturales />} />
+          <Route path="ciclo-del-agua" element={<CicloDelAgua />} />
+          <Route path="ciencias-sociales" element={<CienciasSociales />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
   );
 }
